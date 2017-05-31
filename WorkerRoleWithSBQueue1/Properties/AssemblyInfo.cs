f9d16bf438be42497c767513e6d23465a6665ffc
Vitaliy <v.v.hudenko@gmail.com>
2017-05-31 18:44:22 +0300

    New project

---
+++ WorkerRoleWithSBQueue1/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения о сборке управляются следующим набором 
+// атрибутов. Измените их значения, чтобы задать сведения
+// о сборке.
+[assembly: AssemblyTitle("WorkerRoleWithSBQueue1")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("WorkerRoleWithSBQueue1")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Задание значения false для ComVisible делает типы в этой сборке невидимыми 
+// для компонентов COM.  Для доступа к типу из этой сборки в 
+// COM задайте для атрибута ComVisible этого типа значение true.
+[assembly: ComVisible(false)]
+
+// Следующий GUID — это идентификатор библиотеки типов, если проект предоставляется в COM
+[assembly: Guid("f6928576-5ced-4e63-ae99-e371cfc89470")]
+
+// Сведения о версии сборки включает следующие четыре значения:
+//
+//      Основная версия
+//      Дополнительная версия 
+//      Номер построения
+//      Редакция
+//
+// Вы можете указать все значения или только номера построения и редакции, 
+// используя заполнитель "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ WorkerRoleWithSBQueue1/WorkerRole.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Net;
+using System.Threading;
+using Microsoft.ServiceBus;
+using Microsoft.ServiceBus.Messaging;
+using Microsoft.WindowsAzure;
+using Microsoft.WindowsAzure.ServiceRuntime;
+
+namespace WorkerRoleWithSBQueue1
+{
+    public class WorkerRole : RoleEntryPoint
+    {
+        // Имя вашей очереди
+        const string QueueName = "ProcessingQueue";
+
+        // QueueClient является потокобезопасным. Рекомендуется его кэшировать, 
+        // а не создавать заново при каждом запросе
+        QueueClient Client;
+        ManualResetEvent CompletedEvent = new ManualResetEvent(false);
+
+        public override void Run()
+        {
+            Trace.WriteLine("Начало обработки сообщений");
+
+            // Запускает конвейер сообщений, и для каждого полученного сообщения осуществляется обратный вызов. Вызов закрытия клиента приведет к остановке конвейера.
+            Client.OnMessage((receivedMessage) =>
+                {
+                    try
+                    {
+                        // Обработка сообщения
+                        Trace.WriteLine("Обработка сообщения Service Bus: " + receivedMessage.SequenceNumber.ToString());
+                    }
+                    catch
+                    {
+                        // Здесь обрабатываются любые исключения, связанные с обработкой сообщений
+                    }
+                });
+
+            CompletedEvent.WaitOne();
+        }
+
+        public override bool OnStart()
+        {
+            // Установка максимального количества одновременных подключений 
+            ServicePointManager.DefaultConnectionLimit = 12;
+
+            // Создание очереди, если она еще не существует
+            string connectionString = CloudConfigurationManager.GetSetting("Microsoft.ServiceBus.ConnectionString");
+            var namespaceManager = NamespaceManager.CreateFromConnectionString(connectionString);
+            if (!namespaceManager.QueueExists(QueueName))
+            {
+                namespaceManager.CreateQueue(QueueName);
+            }
+
+            // Инициализация подключения к очереди Service Bus
+            Client = QueueClient.CreateFromConnectionString(connectionString, QueueName);
+            return base.OnStart();
+        }
+
+        public override void OnStop()
+        {
+            // Закрытие подключения к очереди Service Bus
+            Client.Close();
+            CompletedEvent.Set();
+            base.OnStop();
+        }
+    }
+}
